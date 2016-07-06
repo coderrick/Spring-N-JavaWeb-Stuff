1c02891bd76aa502dae6b230e54f616f7ccbc6cd
coderrick <derrick.duncan@temple.edu>
2016-07-05 23:55:20 -0400

    edited code to by example servlet

--- src/main/com/derricks/examples/web/WelcomeController.java
+++ src/main/com/derricks/examples/web/WelcomeController.java
@@ -1,53 +1,21 @@
 package com.derricks.examples.web;
 
-import java.util.Map;
+import java.io.IOException;
+import java.util.Date;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import com.derricks.examples.service.HelloWorldService;
+public class ExampleServlet extends HttpServlet {
 
-@Controller
-public class WelcomeController {
+    private static final long serialVersionUID = 1L;
 
-	private final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
-	private final HelloWorldService helloWorldService;
-
-	@Autowired
-	public WelcomeController(HelloWorldService helloWorldService) {
-		this.helloWorldService = helloWorldService;
-	}
-
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index(Map<String, Object> model) {
-
-		logger.debug("index() is executed!");
-
-		model.put("title", helloWorldService.getTitle(""));
-		model.put("msg", helloWorldService.getDesc());
-		
-		return "index";
-	}
-
-	@RequestMapping(value = "/hello/{name:.+}", method = RequestMethod.GET)
-	public ModelAndView hello(@PathVariable("name") String name) {
-
-		logger.debug("hello() is executed - $name {}", name);
-
-		ModelAndView model = new ModelAndView();
-		model.setViewName("index");
-		
-		model.addObject("title", helloWorldService.getTitle(name));
-		model.addObject("msg", helloWorldService.getDesc());
-		
-		return model;
-
-	}
-
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("currentDate", new Date());
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+	
 }
